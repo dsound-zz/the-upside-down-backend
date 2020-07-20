dfb35f0c476fcb2b12141569354b1a27441388ea
dsound-zz <demiansims@gmail.com>
2020-07-20 19:34:05 -0400

    fixed serializer method in controllers

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -7,7 +7,7 @@ class Api::V1::GamesController < ApplicationController
 
   def show
     @game = Game.find(params[:id])
-    render json: GameSerializer.new(@game), status: :ok
+    render json: @game, status: :ok
   end
 
   def create
